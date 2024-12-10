72e3aa996fc1796b93d3380f00ac6de9b2360776
Charly charly <cadiazp@gmail.com>
2024-12-10 10:21:26 -0300

    Algoritmo para calcular el área de un círculo en base al diámetro

---
+++ calculo_area.py
@@ -0,0 +1,9 @@
+from math import pi
+
+Diametro = float(input("Por favor ingrese el díametro del círculo: "))
+
+radio = Diametro / 2
+
+Area = pi * radio**2
+
+print("El área del circulo es: ", Area)
